fde1d48fe7c031f59ef05e1d453530e3648d07ca
Ecapc <emersoncapa@gmail.com>
2023-05-22 18:29:07 -0600

    Ingreso en ColorProduct

--- database/seeders/ColorSeeder.php
+++ database/seeders/ColorSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Color;
+
 class ColorSeeder extends Seeder
 {
     /**
@@ -12,6 +14,12 @@ class ColorSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $colors = ['white', 'blue', 'gray', 'red'];
+
+        foreach ($colors as $color) {
+            Color::create([
+                'name' => $color
+            ]);
+        }
     }
 }
